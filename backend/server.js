63ec65037197b8b5ba74ea2eeef9618720e59f15
Eng Mahmoud Majed <mahmoudmajed404@gmail.com>
2025-11-01 08:02:53 +0200

    start

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import {DB_URI,PORT} from './config/config.js';
+
+const app = express();
+
+mongoose.connect(DB_URI)
+  .then(r => console.log(`MongoDB connected Successfully`))
+  .catch((err) => console.log(err));
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!').status(200).end();
+})
+app.get('/api/v1/test', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data:{
+      product:"hello world"
+    }
+  }).end();
+})
+
+app.listen(PORT, () => {
+  console.log(`app listening on port ${PORT}`)
+})
